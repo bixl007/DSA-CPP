e5343360f20fce8ce27e377811ea873acae79956
Bishal Baira <bishalbaira432@gmail.com>
2024-06-01 20:09:34 +0530

    Feat: Files Renamed

---
+++ 005_reverseArray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+    int end = size - 1;
+    int start = 0;
+
+    while (start != end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    int size, key;
+    cout << "Enter the size: ";
+    cin >> size;
+    cout << endl;
+
+    int arr[size];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << endl;
+    reverseArray(arr, size);
+}
