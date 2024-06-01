788f7d426d8366a29f143949cff7230336e68a9c
Bishal Baira <bishalbaira432@gmail.com>
2024-06-01 16:40:56 +0530

    Feat: Files modified

--- 001_arrayBasics.cpp
+++ 001_arrayBasics.cpp
@@ -1,30 +1,29 @@
 #include <iostream>
 using namespace std;
 
-void outputArray(int arr[], int size){
-    cout << "The elements are: "<< endl;
+void outputArray(int arr[], int size)
+{
+    cout << "The elements are: " << endl;
     for (int i = 0; i < size; i++)
     {
-        cout << arr[i] << " " ;
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-int main(){
+int main()
+{
     int size;
     cout << "Enter the size of your array: ";
     cin >> size;
     int arr[size];
-    
-    cout << "Enter the elements of your array: ";
 
+    cout << "Enter the elements of your array: ";
 
     for (int i = 0; i < size; i++)
     {
         cin >> arr[i];
     }
 
-    outputArray(arr,size);
-
-
+    outputArray(arr, size);
 }
