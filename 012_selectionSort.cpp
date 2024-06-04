3ad8aea984c90265c8a862918052e45f8eb4a6e3
Bishal Baira <bishalbaira432@gmail.com>
2024-06-04 16:20:41 +0530

    Feat: Files Added

---
+++ 012_selectionSort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        swap(arr[minIndex], arr[i]);
+    }
+}
+
+void outputArray(int arr[], int size)
+{
+    cout << "The sorted elements are: " ;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    selectionSort(arr, n);
+    outputArray(arr, n);
+}
