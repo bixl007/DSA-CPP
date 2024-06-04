12dd64961544691395654ad4778f545ef4d03d0f
Bishal Baira <bishalbaira432@gmail.com>
2024-06-04 17:08:18 +0530

    Feat: Files Added

---
+++ 013_bubbleSort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{   
+    bool swapped = false;
+    for(int i = 1; i < n; i++){
+        for(int j = 0; j < n-i; j++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(!swapped) break;
+    }
+}
+
+
+void outputArray(int arr[], int size)
+{
+    cout << "The sorted elements are: " ;
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+    outputArray(arr, n);
+}
