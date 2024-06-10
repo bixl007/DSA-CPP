8fd1bf3f5cb0b4fb27c98ea6da3863071ad09e7b
Bishal Baira <bishalbaira432@gmail.com>
2024-06-10 18:35:21 +0530

    feat: Files Added

---
+++ 018_palindromeString.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int palindromeString(char str[], int n)
+{
+    int i = 0;
+    int j = n - 1;
+    while (i <= j)
+    {
+        if (str[i] != str[j])
+        {
+            return 0;
+        }
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return 1;
+}
+
+int countLength(string str)
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+int main()
+{
+
+    char ch[100];
+
+    cout << "Enter your String: ";
+    cin >> ch;
+    cout << "Your String is: " << ch << endl;
+    int len = countLength(ch);
+    int ans = palindromeString(ch, countLength(ch));
+    if (ans == 1)
+    {
+        cout << "Palindrome" << endl;
+    }
+    else
+    {
+        cout << "Not Palindrome!" << endl;
+    }
+}
