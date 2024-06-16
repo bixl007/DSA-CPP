feb570a409a4333d204f6325bc305eb396866621
Bishal Baira <bishalbaira432@gmail.com>
2024-06-16 17:57:03 +0530

    Feat: 945. Minimum Increment to Make Array Unique added

---
+++ 26_makeUniqueArray.cpp
@@ -0,0 +1,36 @@
+// This code represent my solution for the leetcode question of 945. Minimum Increment to Make Array Unique
+
+class Solution {
+
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        int count = 0;
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            if (nums[i] == nums[i - 1]) {
+
+                int diff = 1;
+
+                count += 1;
+
+                nums[i] = nums[i] + diff;
+
+            }
+
+            else if (nums[i] < nums[i - 1]) {
+
+                int diff = abs(nums[i] - nums[i - 1]) + 1;
+
+                count += diff;
+
+                nums[i] = nums[i] + diff;
+            }
+        }
+
+        return count;
+    }
+};
