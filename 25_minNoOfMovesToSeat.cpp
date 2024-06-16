73d02eb5b5578c068c1f882d30ebd9f9ebb44fbe
Bishal Baira <bishalbaira432@gmail.com>
2024-06-16 17:54:10 +0530

    Feat: Minimum Number of Moves to Seat Everyone

---
+++ 25_minNoOfMovesToSeat.cpp
@@ -0,0 +1,27 @@
+// This code represent my solution for the leetcode question of 2037. Minimum Number of Moves to Seat Everyone
+
+
+
+class Solution {
+
+public:
+
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+
+       sort(seats.begin(), seats.end());
+
+       sort(students.begin(), students.end());
+
+        int moves = 0;
+
+        for( int i=0; i<seats.size(); i++)
+
+            moves += abs(seats[i] - students[i]);
+
+        
+
+        return moves;
+
+    }
+
+};
