2fd74554db1962dfc1c2eeef10a926fd18f5283d
Bishal Baira <bishalbaira432@gmail.com>
2024-06-20 20:15:47 +0530

    Feat column sum file added

---
+++ 31_colSum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void colSum(int arr[][2], int n, int m)
+{
+    int s = 0;
+    for (int i = 0; i < n; i++)
+    {
+        s = 0;
+        for (int j = 0; j < m; j++)
+        {
+            s += arr[j][i];
+        }
+        cout << s << endl;
+    }
+}
+
+int main()
+{
+    int arr[2][2] = {{4, 8}, {8, 6}};
+
+    cout << "The 2-d array is: " << endl;
+
+    for (int i = 0; i < 2; ++i)
+    {
+        for (int j = 0; j < 2; ++j)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    colSum(arr, 2, 2);
+}
