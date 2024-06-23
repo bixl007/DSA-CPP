c9f4d8163c29b7f7b68155325e032749d14826f8
Bishal Baira <bishalbaira432@gmail.com>
2024-06-23 08:33:42 +0530

    Feat: sorted rows search leetcode added

---
+++ 36_sortedRowSearch.cpp
@@ -0,0 +1,39 @@
+// leetcode 240. Search a 2D Matrix II
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>> &matrix, int target)
+    {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int rowIndex = 0;
+        int colIndex = col - 1;
+
+        while (rowIndex < row && colIndex >= 0)
+        {
+            int element = matrix[rowIndex][colIndex];
+
+            if (element == target)
+            {
+                return 1;
+            }
+            else if (element < target)
+            {
+                rowIndex++;
+            }
+            else
+            {
+                colIndex--;
+            }
+        }
+
+        return 0;
+    }
+};
