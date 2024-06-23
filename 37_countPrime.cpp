d93854bbfbbb5af165e9a4505268c31bb910fcb1
Bishal Baira <bishalbaira432@gmail.com>
2024-06-23 09:25:05 +0530

    Feat: Count prime leetcode added

---
+++ 37_countPrime.cpp
@@ -0,0 +1,32 @@
+// leetcode 204. Count Primes
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    int countPrimes(int n)
+    {
+        int count = 0;
+        vector<bool> prime(n + 1, true);
+
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i < n; i++)
+        {
+            if (prime[i])
+            {
+                count++;
+
+                for (int j = 2 * i; j < n; j += i)
+                {
+                    prime[j] = 0;
+                }
+            }
+        }
+        return count;
+    }
+};
