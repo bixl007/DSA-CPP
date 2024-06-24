728e0c9b087616a0495a97135717cf596de42a2e
Bishal Baira <bishalbaira432@gmail.com>
2024-06-24 18:55:39 +0530

    Feat: Intro to pointer added

---
+++ 41_pointerInter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p = 5;
+    int *ptr = &p;
+    cout << "Value of original variable is :" << p << endl;
+    cout << "Value of pointer is :" << *ptr << endl;
+    cout << "Value of address of pointer is :" << ptr << endl;
+}
