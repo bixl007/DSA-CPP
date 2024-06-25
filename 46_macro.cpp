c1b64140a83abf6245f317356a1fcf254408ecb7
Bishal Baira <bishalbaira432@gmail.com>
2024-06-25 19:26:19 +0530

    Feat: macro added

---
+++ 46_macro.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+#define PI 3.12
+
+int main()
+{
+    int r = 5;
+    double area = PI * r * r;
+    cout << "Area is : " << area << endl;
+
+    return 0;
+}
