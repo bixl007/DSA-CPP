2834adaafbde6ff096b86a5434a84baa645bde99
Bishal Baira <bishalbaira432@gmail.com>
2024-06-25 08:39:39 +0530

    Feat: 2D array using dynamic memory allocation added

---
+++ 44_2dDynamicArray.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int **arr = new int *[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = new int[n];
+    }
+
+    cout << "Enter the elements of 2D array" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "The elements of the array are" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
