f1e1d65d727a55d3a1a2b2421c38cf08a22e3975
Bishal Baira <bishalbaira432@gmail.com>
2024-06-26 08:19:18 +0530

    Feat: fibonacci using recursion added

---
+++ 52_fibonacciRecursion.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return fib(n - 1) + fib(n - 2);
+    // cout << n;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number: " << endl;
+    cin >> n;
+    cout << endl;
+    int ans = fib(n);
+    cout << ans << endl;
+}
