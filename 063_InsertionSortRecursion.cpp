23a401d47f09299aa4e83cfcad67268f23f8f567
Bishal Baira <bishalbaira432@gmail.com>
2024-06-27 11:17:30 +0530

    Feat: Insertion sort using recursion added

---
+++ 063_InsertionSortRecursion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int arr[], int start, int size)
+{
+    if (start >= size)
+        return;
+
+    int temp = arr[start], i;
+
+    for (i = start - 1; i >= 0; i--)
+    {
+        if (arr[i] > temp)
+            arr[i + 1] = arr[i];
+
+        else
+            break;
+    }
+    arr[i + 1] = temp;
+
+    insertionSort(arr, start + , size);
+}
+
+int main()
+{
+    int arr[5] = {5, 61, 8, 1, 2};
+    insertionSort(arr, 1, 5);
+
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
