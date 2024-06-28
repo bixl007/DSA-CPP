f75c14eef654b550337991eaf394a4c4c9687ffb
Bishal Baira <bishalbaira432@gmail.com>
2024-06-28 19:23:45 +0530

    Feat: quick sort using recursion added

---
+++ 065_quickSortRecursion.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int partition(int arr[], int s, int e)
+{
+
+    int pivot = arr[s];
+
+    int count = 0;
+
+    for (int i = s + 1; i < e; i++)
+    {
+        if (arr[i] < pivot)
+            count++;
+    }
+
+    int pivotIndex = count + s;
+
+    swap(arr[s], arr[pivotIndex]);
+
+    int i = s, j = e;
+
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] <= pivot)
+        {
+            i++;
+        }
+        while (arr[j] >= pivot)
+        {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex)
+            swap(arr[i++], arr[j--]);
+    }
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int s, int e)
+{
+    if (s >= e)
+        return;
+
+    int p = partition(arr, s, e);
+
+    // Left Part
+    quickSort(arr, s, p - 1);
+    // Right Part
+    quickSort(arr, p + 1, e);
+}
+
+int main()
+{
+
+    int arr[10] = {7, 1, 2, 5, 4, 9, 6, 3, 4, 2};
+    int n = 10;
+
+    quickSort(arr, 0, n - 1);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
