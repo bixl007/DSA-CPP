6eb06c3925901049921b7744593565fd0d61d614
Bishal Baira <bishalbaira432@gmail.com>
2024-06-29 19:23:01 +0530

    Feat: Assignment Copy constructor in OOPs added

--- 070_copyConstructor.cpp
+++ 070_copyConstructor.cpp
@@ -42,4 +42,17 @@ int main()
     Hero h(10, "Ramesh");
     Hero Ramesh(h);
     cout << Ramesh.name << " has health: " << Ramesh.getHealth() << endl;
+
+    Hero h1(20, "xyz");
+    Hero h2(45, "abc");
+    cout << h1.name << " has health: " << h1.getHealth() << endl;
+    cout << h2.name << " has health: " << h2.getHealth() << endl;
+
+    h1 = h2;
+    cout << h1.name << " has health: " << h1.getHealth() << endl;
+    cout << h2.name << " has health: " << h2.getHealth() << endl;
+
+    h1.name[0] = 'A';
+    cout << h1.name << " has health: " << h1.getHealth() << endl;
+    cout << h2.name << " has health: " << h2.getHealth() << endl;
 }
