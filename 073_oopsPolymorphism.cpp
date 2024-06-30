8f98b4739b50cad2068d6ed6d655c54619c6d502
Bishal Baira <bishalbaira432@gmail.com>
2024-06-30 08:13:53 +0530

    Feat: operator overloading added

--- 073_oopsPolymorphism.cpp
+++ 073_oopsPolymorphism.cpp
@@ -32,3 +32,39 @@ int main()
 }
 */
 
+//                                           operator overloading
+
+/*
+class B
+{
+public:
+    int a;
+    int b;
+
+public:
+    int add()
+    {
+        return a + b;
+    }
+
+    void operator+(B &obj)
+    {
+        // int value1 = this->a;
+        // int value2 = obj.a;
+        // cout << "Output is: " << value2 - value1 << endl;
+
+        cout << "Operator overloading occurred" << endl;
+    }
+};
+
+int main()
+{
+
+    B obj1, obj2;
+
+    obj1.a = 4;
+    obj2.a = 7;
+
+    obj1 + obj2;
+}
+*/
