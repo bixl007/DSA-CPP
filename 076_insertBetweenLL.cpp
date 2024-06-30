663e4ec4488a32e5af778c04e9e991420b12f5a5
Bishal Baira <bishalbaira432@gmail.com>
2024-06-30 15:12:32 +0530

    Feat: Insert element at a particular position of a linked list added

---
+++ 076_insertBetweenLL.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(Node *&tail, Node *&head, int position, int d)
+{
+    if (position == 1)
+    {
+        insertAtHead(head, d);
+        return;
+    }
+
+    Node *temp = head;
+    int cnt = 1;
+
+    while (cnt < position - 1)
+    {
+        temp = temp->next;
+        cnt++;
+    }
+
+    // Inserting at last position
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    // Creating a node for d
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    cout << "Before inserting " << endl;
+    Node *node2 = new Node(40);
+    Node *head = node2;
+    Node *tail = node2;
+    insertAtTail(tail, 50);
+    insertAtTail(tail, 60);
+    insertAtTail(tail, 70);
+    insertAtTail(tail, 80);
+    insertAtTail(tail, 90);
+    print(head);
+
+    cout << "After inserting" << endl;
+    insertAtPosition(tail, head, 4, 65);
+    print(head);
+
+    return 0;
+}
