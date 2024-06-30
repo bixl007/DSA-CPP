d73c9359cb4f17c7f7d635ca9a7e58ff08792847
Bishal Baira <bishalbaira432@gmail.com>
2024-06-30 08:07:15 +0530

    Feat: OOPs Polymorphism - function overloading added

---
+++ 073_oopsPolymorphism.cpp
@@ -0,0 +1,34 @@
+
+
+#include <iostream>
+using namespace std;
+
+//                                 Function overloading
+/*
+class A
+{
+public:
+    void sayHello()
+    {
+        cout << "Hello" << endl;
+    }
+    int sayHello(char name)
+    {
+        cout << "Hi there" << endl;
+        return 1;
+    }
+    void sayHello(string name)
+    {
+        cout << "Hey" << endl;
+    }
+};
+
+int main()
+{
+    A obj;
+    obj.sayHello();
+
+    return 0;
+}
+*/
+
