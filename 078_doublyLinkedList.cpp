e0a56ea3b5c3be42c7b157a5854836fcdf7b0036
Bishal Baira <bishalbaira432@gmail.com>
2024-07-01 10:23:18 +0530

    Feat: delete element from doubly LL added

--- 078_doublyLinkedList.cpp
+++ 078_doublyLinkedList.cpp
@@ -14,6 +14,15 @@ public:
         this->prev = NULL;
         this->next = NULL;
     }
+    ~Node()
+    {
+        int value = this->data;
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+    }
 };
 
 // Traversing LL
@@ -45,21 +54,40 @@ int getLength(Node *&head)
 }
 
 // Insert to head
-void insertAtHead(Node *&head, int d)
+void insertAtHead(Node *&head, Node *&tail, int d)
 {
-    Node *temp = new Node(d);
-    temp->next = head;
-    head->prev = temp;
-    head = temp;
+    if (head == NULL)
+    {
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+        Node *temp = new Node(d);
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
 }
 
 // Insert to tail
-void insertAtTail(Node *&tail, int d)
+void insertAtTail(Node *&head, Node *&tail, int d)
 {
-    Node *temp = new Node(d);
-    temp->prev = tail;
-    tail->next = temp;
-    tail = temp;
+    if (tail == NULL)
+    {
+        Node *temp = new Node(d);
+        head = temp;
+        tail = temp;
+    }
+    else
+    {
+
+        Node *temp = new Node(d);
+        temp->prev = tail;
+        tail->next = temp;
+        tail = temp;
+    }
 }
 
 // Insert at a position
@@ -67,7 +95,7 @@ void insertAtPosition(Node *&tail, Node *&head, int position, int d)
 {
     if (position == 1)
     {
-        insertAtHead(head, d);
+        insertAtHead(head, tail, d);
         return;
     }
 
@@ -83,7 +111,7 @@ void insertAtPosition(Node *&tail, Node *&head, int position, int d)
     // Inserting at last position
     if (temp->next == NULL)
     {
-        insertAtTail(tail, d);
+        insertAtTail(head, tail, d);
         return;
     }
 
@@ -96,6 +124,46 @@ void insertAtPosition(Node *&tail, Node *&head, int position, int d)
     nodeToInsert->prev = temp;
 }
 
+// Deleting a node
+void deleteNode(int position, Node *&head, Node *&tail)
+{
+
+    // Deleting first or start node
+    if (position == 1)
+    {
+        Node *temp = head;
+        temp->next->prev = NULL;
+        head = temp->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    // Deleting any middle node or last node
+    else
+    {
+        Node *curr = head;
+        Node *prev = NULL;
+
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        curr->prev = NULL;
+        prev->next = curr->next;
+        curr->next = NULL;
+
+        delete curr;
+        if (prev->next == NULL)
+        {
+            tail = prev;
+        }
+    }
+}
+
 int main()
 {
 
@@ -105,25 +173,30 @@ int main()
     cout << "After inserting to head: " << endl;
     print(head);
     cout << "Length of the Linked list is: " << getLength(head) << endl;
-    insertAtHead(head, 24);
+    insertAtHead(head, tail, 24);
     print(head);
     cout << "Length of the Linked list is: " << getLength(head) << endl;
-    insertAtHead(head, 28);
-    insertAtHead(head, 94);
-    insertAtHead(head, 70);
-    insertAtHead(head, 65);
+    insertAtHead(head, tail, 28);
+    insertAtHead(head, tail, 94);
+    insertAtHead(head, tail, 70);
+    insertAtHead(head, tail, 65);
     print(head);
     cout << "Length of the Linked list is: " << getLength(head) << endl;
 
     cout << "After inserting to tail: " << endl;
-    insertAtTail(tail, 74);
-    insertAtTail(tail, 62);
-    insertAtTail(tail, 47);
-    insertAtTail(tail, 56);
+    insertAtTail(head, tail, 74);
+    insertAtTail(head, tail, 62);
+    insertAtTail(head, tail, 47);
+    insertAtTail(head, tail, 56);
     print(head);
     cout << "Length of the Linked list is: " << getLength(head) << endl;
 
     insertAtPosition(tail, head, 4, 65);
+    print(head);
+    cout << "Length of the Linked list is: " << getLength(head) << endl;
+
+    cout << "Deleting a node" << endl;
+    deleteNode(2, head, tail);
     print(head);
     cout << "Length of the Linked list is: " << getLength(head) << endl;
 
