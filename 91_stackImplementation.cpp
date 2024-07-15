549735f03af49f05b487593d860d8d012977f3f2
Bishal Baira <bishalbaira432@gmail.com>
2024-07-15 20:13:12 +0530

    Feat: stack implementation using class

---
+++ 91_stackImplementation.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+public:
+    int *arr;
+    int top;
+    int size;
+
+    Stack(int size)
+    {
+        this->size = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    void push(int element)
+    {
+        if (size - top > 1)
+        {
+            top++;
+            arr[top] = element;
+        }
+        else
+        {
+            cout << "Stack Overflow" << endl;
+        }
+    }
+
+    void pop()
+    {
+        if (top >= 0)
+        {
+            top--;
+        }
+        else
+        {
+            cout << "Stack Underflow" << endl;
+        }
+    }
+
+    int peek()
+    {
+        if (top >= 0)
+            return arr[top];
+        else
+        {
+            cout << "Stack is empty" << endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty()
+    {
+        return top == -1;
+    }
+};
+
+int main()
+{
+    Stack st(5);
+
+    st.push(22);
+    st.push(45);
+    st.push(68);
+    st.push(12);
+    st.push(9);
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    st.pop();
+
+    cout << st.peek() << endl;
+
+    if (st.isEmpty())
+    {
+        cout << "Stack is empty" << endl;
+    }
+
+    else
+    {
+        cout << "Stack is not empty" << endl;
+    }
+}
