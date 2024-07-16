c58ba3cb807b12704cd38201f98742a0cfe2bf2c
Bishal Baira <bishalbaira432@gmail.com>
2024-07-16 19:17:54 +0530

    Feat: delete middle element from stack

---
+++ 094_deleteStackMiddle.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h> 
+#include <stack>
+using namespace std;
+
+void solve(stack<int>&inputStack, int count, int size) {
+   if(count == size / 2) {
+      inputStack.pop();
+      return ;
+   }
+
+   int num = inputStack.top();
+   inputStack.pop();
+
+   solve(inputStack, count + 1, size);
+   
+   inputStack.push(num);
+
+}
+
+void deleteMiddle(stack<int>&inputStack, int N){
+	
+   int count = 0;
+   solve(inputStack, count, N);
+   
+}
