06ecb36920c118bb1226d0f8d915da4ec78df138
Bishal Baira <bishalbaira432@gmail.com>
2024-07-29 19:53:20 +0530

    Feat: LCA of two nodes

---
+++ 151_LCAOf2Node.cpp
@@ -0,0 +1,30 @@
+/************************************************************
+    Following is the Binary Search Tree node structure
+
+    class TreeNode
+    {
+    public:
+        int data;
+        TreeNode *left, *right;
+        TreeNode() : data(0), left(NULL), right(NULL) {}
+        TreeNode(int x) : data(x), left(NULL), right(NULL) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : data(x), left(left), right(right) {}
+    };
+
+***********************************************************
+
+
+
+TreeNode *LCAinaBST(TreeNode *root, TreeNode *P, TreeNode *Q)
+{
+    if(root == NULL) 
+        return NULL;
+    
+    if(root -> data < P -> data && root -> data < Q -> data)
+        return LCAinaBST(root -> right, P, Q);
+    if(root -> data > P -> data && root -> data > Q -> data)
+        return LCAinaBST(root -> left, P, Q);
+
+    return root;
+
+}*/
