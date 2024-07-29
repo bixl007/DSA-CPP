2ac4c4a7987c075239c81bb43e48f7b2b1c9bb07
Bishal Baira <bishalbaira432@gmail.com>
2024-07-29 22:00:25 +0530

    Feat: BST two sum

---
+++ 155_BSTtwoSum.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+// Following is the Binary Tree node structure
+/**************
+class BinaryTreeNode {
+    public :
+    T data;
+    BinaryTreeNode<T> *left;
+    BinaryTreeNode<T> *right;
+
+    BinaryTreeNode(T data) {
+        this -> data = data;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+***************
+
+void inorder(BinaryTreeNode<int>* root, vector<int> &in) {
+    if(root == NULL)
+        return ;
+
+    inorder(root -> left, in);
+    in.push_back(root -> data);
+    inorder(root -> right, in);
+}
+
+bool twoSumInBST(BinaryTreeNode<int>* root, int target) {
+    //Write your code here
+
+    vector<int> inorderVal;
+    inorder(root, inorderVal);
+
+    int i = 0, j = inorderVal.size() - 1;
+
+    while(i < j) {
+        int sum = inorderVal[i] + inorderVal[j];
+        if(sum == target)
+            return true;
+        else if(sum > target)
+            j--;
+        else
+            i++;
+    }
+    return false;
+}
+
+*/
