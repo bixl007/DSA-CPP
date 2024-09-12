ace8417b24687476c7b62dc09cde369845feb222
Bishal Baira <bishalbaira432@gmail.com>
2024-09-12 22:38:13 +0530

    count the number of consisstent strings

---
+++ 204_countConsistentStr.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words) {
+        int arr[26] = {0};
+        for (char c : allowed) {
+            arr[c - 'a'] = 1;
+        }
+
+        int count = 0;
+        for (string word : words) {
+            int flag = 1;
+            for (char c : word) {
+                if (arr[c - 'a'] == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+            count += flag;
+        }
+        return count;
+    }
+};
