ae616e179501c66347f3cd1d5ddfd61dd011270b
Bishal Baira <bishalbaira432@gmail.com>
2024-09-30 22:54:27 +0530

    Sort by parity II

---
+++ 220_sortByParityII.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortArrayByParityII(vector<int> &nums)
+    {
+        vector<int> even, odd;
+        for (auto i : nums)
+        {
+            if (i % 2 == 0)
+            {
+                even.push_back(i);
+            }
+            else
+            {
+                odd.push_back(i);
+            }
+        }
+        int c1 = 0, c2 = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i % 2 == 0)
+            {
+                nums[i] = even[c1++];
+            }
+            else
+            {
+                nums[i] = odd[c2++];
+            }
+        }
+        return nums;
+    }
+};
