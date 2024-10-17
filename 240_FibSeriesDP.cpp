d2d45dd08b0fb2c5c12cb8517cad476654cb67cf
Bishal Baira <bishalbaira432@gmail.com>
2024-10-17 18:29:37 +0530

    Fibonacci Series using Dp space optimisation approach

--- 240_FibSeriesDP.cpp
+++ 240_FibSeriesDP.cpp
@@ -35,6 +35,7 @@ int main()
 */
 
 // Bottom - up => Tabulation
+/*
 int main()
 {
     int n;
@@ -55,5 +56,34 @@ int main()
 
     cout << dp[n] << endl;
 
+    return 0;
+}
+*/
+
+// Space Optimisation
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int prev1 = 1;
+    int prev2 = 0;
+
+    if (n == 0)
+    {
+        cout << prev2 << endl;
+        return 0;
+    }
+
+    for (int i = 2; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    cout << prev1 << endl;
     return 0;
 }
