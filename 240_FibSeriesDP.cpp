bf5626f9ce5fb01fa50ad71ed12c875a3a033c08
Bishal Baira <bishalbaira432@gmail.com>
2024-10-17 18:22:26 +0530

    Fibonacci Series using Dp bottom up approach

--- 240_FibSeriesDP.cpp
+++ 240_FibSeriesDP.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Top - Down => Memorisation
+/*
 int fib(int n, vector<int> &dp)
 {
     if (n <= 1)
@@ -28,6 +29,31 @@ int main()
     }
 
     cout << fib(n, dp) << endl;
+
+    return 0;
+}
+*/
+
+// Bottom - up => Tabulation
+int main()
+{
+    int n;
+    cin >> n;
+
+    // Step 1
+    vector<int> dp(n + 1);
+
+    // Step 2
+    dp[1] = 1;
+    dp[0] = 0;
+
+    // Step 3
+    for (int i = 2; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    cout << dp[n] << endl;
 
     return 0;
 }
