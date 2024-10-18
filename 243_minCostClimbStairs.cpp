ac414fc6bfe70e39d25e3508a2df960cb46f699c
Bishal Baira <bishalbaira432@gmail.com>
2024-10-18 20:07:45 +0530

    Min Cost Climbing Stairs - Top Down Approach

--- 243_minCostClimbStairs.cpp
+++ 243_minCostClimbStairs.cpp
@@ -28,3 +28,35 @@ public:
         return ans;
     }
 };
+
+// Top - Down Approach
+class Solution
+{
+private:
+    int solve(vector<int> &cost, int n, vector<int> &dp)
+    {
+        if (n == 0)
+            return cost[0];
+
+        if (n == 1)
+            return cost[1];
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        dp[n] = cost[n] + min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+
+        return dp[n];
+    }
+
+public:
+    int minCostClimbingStairs(vector<int> &cost)
+    {
+        int n = cost.size();
+        vector<int> dp(n + 1, -1);
+
+        int ans = min(solve(cost, n - 1, dp), solve(cost, n - 2, dp));
+
+        return ans;
+    }
+};
