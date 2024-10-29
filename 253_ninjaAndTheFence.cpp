f8767b1ef89b1a55d398ef67c0bd9d4f6c03c386
Bishal Baira <bishalbaira432@gmail.com>
2024-10-29 16:29:02 +0530

    Ninja and the fence - Tabulation approach

--- 253_ninjaAndTheFence.cpp
+++ 253_ninjaAndTheFence.cpp
@@ -62,3 +62,24 @@ int numberOfWays(int n, int k)
     vector<int> dp(n + 1, -1);
     return solve(n, k, dp);
 }
+
+// Tabulation
+int solve(int n, int k)
+{
+
+    vector<int> dp(n + 1, 0);
+
+    dp[1] = k;
+    dp[2] = add(k, mul(k, k - 1));
+
+    for (int i = 3; i <= n; i++)
+    {
+        dp[i] = add(mul(dp[i - 2], k - 1), mul(dp[i - 1], k - 1));
+    }
+    return dp[n];
+}
+
+int numberOfWays(int n, int k)
+{
+    return solve(n, k);
+}
