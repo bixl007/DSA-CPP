0aa07534439b6092db8f2e60e8048dbc85df6af9
Bishal Baira <bishalbaira432@gmail.com>
2024-10-29 16:31:29 +0530

    Ninja and the fence - Space optimisation

--- 253_ninjaAndTheFence.cpp
+++ 253_ninjaAndTheFence.cpp
@@ -83,3 +83,24 @@ int numberOfWays(int n, int k)
 {
     return solve(n, k);
 }
+
+// Space optimisation
+int solve(int n, int k)
+{
+
+    int prev2 = k;
+    int prev1 = add(k, mul(k, k - 1));
+
+    for (int i = 3; i <= n; i++)
+    {
+        int ans = add(mul(prev2, k - 1), mul(prev1, k - 1));
+        prev2 = prev1;
+        prev1 = ans;
+    }
+    return prev1;
+}
+
+int numberOfWays(int n, int k)
+{
+    return solve(n, k);
+}
