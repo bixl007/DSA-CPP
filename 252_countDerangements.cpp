9cb3193e2d45fdd8adecb07b774dc9c85c40303b
Bishal Baira <bishalbaira432@gmail.com>
2024-10-29 13:55:13 +0530

    Count Derangements - Memorization approach

--- 252_countDerangements.cpp
+++ 252_countDerangements.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define MOD 1000000007
 
+// Recursion
 long long int countDerangements(int n)
 {
     if (n == 1)
@@ -13,4 +14,26 @@ long long int countDerangements(int n)
     int ans = (((n - 1) % MOD) * ((countDerangements(n - 1) % MOD) + (countDerangements(n - 2) % MOD)) % MOD);
 
     return ans;
+}
+
+// Memorization
+long long int solve(int n, vector<long long int> &dp)
+{
+    if (n == 1)
+        return 0;
+    if (n == 2)
+        return 1;
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    dp[n] = (((n - 1) % MOD) * ((solve(n - 1, dp) % MOD) + (solve(n - 2, dp) % MOD)) % MOD);
+    return dp[n];
+}
+
+long long int countDerangements(int n)
+{
+
+    vector<long long int> dp(n + 1, -1);
+    return solve(n, dp);
 }
