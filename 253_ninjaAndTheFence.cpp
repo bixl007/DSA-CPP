b7241ae958f50cbeeacf3068ec327960878f77e8
Bishal Baira <bishalbaira432@gmail.com>
2024-10-29 16:24:51 +0530

    Ninja and the fence - Memorization approach

--- 253_ninjaAndTheFence.cpp
+++ 253_ninjaAndTheFence.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 #define MOD 1000000007
 
-
 // Recursive
 int add(int a, int b)
 {
@@ -30,4 +29,36 @@ int solve(int n, int k)
 int numberOfWays(int n, int k)
 {
     return solve(n, k);
+}
+
+// Memorization
+int add(int a, int b)
+{
+    return ((a % MOD) + (b % MOD)) % MOD;
+}
+
+int mul(int a, int b)
+{
+    return ((a % MOD) * 1LL * (b % MOD)) % MOD;
+}
+
+int solve(int n, int k, vector<int> &dp)
+{
+    if (n == 1)
+        return k;
+
+    if (n == 2)
+        return add(k, mul(k, k - 1));
+
+    if (dp[n] != -1)
+        return dp[n];
+
+    dp[n] = add(mul(solve(n - 2, k, dp), k - 1), mul(solve(n - 1, k, dp), k - 1));
+    return dp[n];
+}
+
+int numberOfWays(int n, int k)
+{
+    vector<int> dp(n + 1, -1);
+    return solve(n, k, dp);
 }
