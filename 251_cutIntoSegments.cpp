5ad9025945cdb35b44533c43afc207bc6009284f
Bishal Baira <bishalbaira432@gmail.com>
2024-10-29 13:19:43 +0530

    Cut into segments - Tabulation approach

--- 251_cutIntoSegments.cpp
+++ 251_cutIntoSegments.cpp
@@ -61,3 +61,27 @@ int cutSegments(int n, int x, int y, int z)
 
     return ans;
 }
+
+// Tabulation
+int cutSegments(int n, int x, int y, int z)
+{
+
+    vector<int> dp(n + 1, INT_MIN);
+
+    dp[0] = 0;
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (i - x >= 0)
+            dp[i] = max(dp[i], dp[i - x] + 1);
+        if (i - y >= 0)
+            dp[i] = max(dp[i], dp[i - y] + 1);
+        if (i - z >= 0)
+            dp[i] = max(dp[i], dp[i - z] + 1);
+    }
+
+    if (dp[n] < 0)
+        return 0;
+
+    return dp[n];
+}
