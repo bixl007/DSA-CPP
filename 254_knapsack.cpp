c9b89e0b9a8e3163428314a1ec2cca72b0e88c32
Bishal Baira <bishalbaira432@gmail.com>
2024-10-30 12:48:40 +0530

    Knapsack - Memorization approach

--- 254_knapsack.cpp
+++ 254_knapsack.cpp
@@ -31,3 +31,42 @@ int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
 {
     return solve(weight, value, n - 1, maxWeight);
 }
+
+// Memorization
+int solve(vector<int> &weight, vector<int> &value, int index, int capacity, vector<vector<int>> &dp)
+{
+    if (index == 0)
+    {
+        if (weight[index] <= capacity)
+        {
+            return value[0];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    if (dp[index][capacity] != -1)
+    {
+        return dp[index][capacity];
+    }
+
+    int include = 0;
+
+    if (weight[index] <= capacity)
+    {
+        include = value[index] + solve(weight, value, index - 1, capacity - weight[index], dp);
+    }
+
+    int exclude = solve(weight, value, index - 1, capacity, dp);
+    dp[index][capacity] = max(include, exclude);
+
+    return dp[index][capacity];
+}
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight)
+{
+    vector<vector<int>> dp(n, vector<int>(maxWeight + 1, -1));
+    return solve(weight, value, n - 1, maxWeight, dp);
+}
