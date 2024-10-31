5aac665396e68d117f1d511ab01fbc497c98b44e
Bishal Baira <bishalbaira432@gmail.com>
2024-10-31 22:50:17 +0530

    Fix: corrected the alignment

--- 013_bubbleSort.cpp
+++ 013_bubbleSort.cpp
@@ -2,23 +2,26 @@
 using namespace std;
 
 void bubbleSort(int arr[], int n)
-{   
+{
     bool swapped = false;
-    for(int i = 1; i < n; i++){
-        for(int j = 0; j < n-i; j++){
-            if(arr[j] > arr[j + 1]){
-                swap(arr[j], arr[j+1]);
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 0; j < n - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        if(!swapped) break;
+        if (!swapped)
+            break;
     }
 }
 
-
 void outputArray(int arr[], int size)
 {
-    cout << "The sorted elements are: " ;
+    cout << "The sorted elements are: ";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
