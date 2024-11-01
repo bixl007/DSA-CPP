c64052e9cc1dabc0904f252987de29637109b8cb
Bishal Baira <bishalbaira432@gmail.com>
2024-11-01 20:06:38 +0530

    Get Minimum Squares - Memorization approach

--- 256_GetMinimumSquares.cpp
+++ 256_GetMinimumSquares.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Recursive
@@ -25,5 +25,37 @@ public:
     int MinSquares(int n)
     {
         return solve(n);
+    }
+};
+
+// Memorization
+class Solution
+{
+public:
+    int solve(int n, vector<int> &dp)
+    {
+        if (n == 0)
+        {
+            return 0;
+        }
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        int ans = n;
+
+        for (int i = 1; i * i <= n; i++)
+        {
+            int temp = i * i;
+            ans = min(ans, 1 + solve(n - temp, dp));
+        }
+        dp[n] = ans;
+        return dp[n];
+    }
+
+    int MinSquares(int n)
+    {
+        vector<int> dp(n + 1, -1);
+        return solve(n, dp);
     }
 };
