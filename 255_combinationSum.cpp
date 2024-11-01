40e13a41225137f0d2d16fe44f379848d54bc831
Bishal Baira <bishalbaira432@gmail.com>
2024-11-01 19:43:36 +0530

    Combination Sum - Memorization approach

--- 255_combinationSum.cpp
+++ 255_combinationSum.cpp
@@ -22,3 +22,34 @@ int findWays(vector<int> &num, int tar)
 {
     return solve(num, tar);
 }
+
+// Memorization
+int solve(vector<int> &num, int tar, vector<int> &dp)
+{
+    if (tar < 0)
+        return 0;
+    if (tar == 0)
+        return 1;
+
+    if (dp[tar] != -1)
+    {
+        return dp[tar];
+    }
+
+    int ans = 0;
+
+    for (int i = 0; i < num.size(); i++)
+    {
+        ans += solve(num, tar - num[i], dp);
+    }
+
+    dp[tar] = ans;
+
+    return dp[tar];
+}
+
+int findWays(vector<int> &num, int tar)
+{
+    vector<int> dp(tar + 1, -1);
+    return solve(num, tar, dp);
+}
