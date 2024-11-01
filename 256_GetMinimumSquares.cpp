c8318f2a26744e4673944a9e1e641aba7799ec0d
Bishal Baira <bishalbaira432@gmail.com>
2024-11-01 20:16:55 +0530

    Get Minimum Squares - Tabulation approach

--- 256_GetMinimumSquares.cpp
+++ 256_GetMinimumSquares.cpp
@@ -59,3 +59,23 @@ public:
         return solve(n, dp);
     }
 };
+
+// Tabulation
+int MinSquares(int n)
+{
+    vector<int> dp(n + 1, INT_MAX);
+
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j * j <= n; j++)
+        {
+            int temp = j * j;
+            if (i - temp >= 0)
+                dp[i] = min(dp[i], 1 + dp[i - temp]);
+        }
+    }
+
+    return dp[n];
+}
