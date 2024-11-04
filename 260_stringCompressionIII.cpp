6602f344504d2fb9eb09ea030c2b86d45807383f
Bishal Baira <bishalbaira432@gmail.com>
2024-11-05 04:07:33 +0530

    String compression III

---
+++ 260_stringCompressionIII.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string compressedString(string word)
+    {
+        string comp = "";
+        int cnt = 1, n = word.size();
+        char ch = word[0];
+        for (int i = 1; i < n; i++)
+        {
+            if (word[i] == ch && cnt < 9)
+                cnt++;
+            else
+            {
+                comp.push_back(cnt + '0');
+                comp.push_back(ch);
+                ch = word[i];
+                cnt = 1;
+            }
+        }
+        comp.push_back(cnt + '0');
+        comp.push_back(ch);
+        return comp;
+    }
+};
