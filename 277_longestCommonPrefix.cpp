fc43a7f7637164b2bdba276ae04443ad0cb64c96
Bishal Baira <bishalbaira432@gmail.com>
2024-11-23 23:33:45 +0530

    Longest Common Prefix

---
+++ 277_longestCommonPrefix.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string longestCommonPrefix(vector<string> &strs)
+    {
+        string ans = "";
+        sort(strs.begin(), strs.end());
+        int n = strs.size();
+        string first = strs[0], last = strs[n - 1];
+        for (int i = 0; i < min(first.size(), last.size()); i++)
+        {
+            if (first[i] != last[i])
+            {
+                return ans;
+            }
+            ans += first[i];
+        }
+        return ans;
+    }
+};
