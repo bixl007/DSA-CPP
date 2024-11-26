734bfd08d533a197a93d2dde768099bc94f467ee
Bishal Baira <151175250+Bishalhere007@users.noreply.github.com>
2024-11-26 21:58:31 +0530

    Merge Two Binary Trees

---
+++ 282_mergeBinaryTree.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution
+{
+public:
+    TreeNode *mergeTrees(TreeNode *root1, TreeNode *root2)
+    {
+        if (root1 == NULL)
+        {
+            return root2;
+        }
+        else if (root2 == NULL)
+        {
+            return root1;
+        }
+
+        root2->left = mergeTrees(root1->left, root2->left);
+        root2->val = root2->val + root1->val;
+        root2->right = mergeTrees(root1->right, root2->right);
+
+        return root2;
+    }
+};
