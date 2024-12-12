47f622d7329fa0c463cdb01e72747460f56c075f
Bishal Baira <bishalbaira432@gmail.com>
2024-12-12 19:06:44 +0530

    Reverse Vowels of a String

---
+++ 297_reverseVowel.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string reverseVowels(string s)
+    {
+        string word = s;
+        int start = 0;
+        int end = s.length() - 1;
+        string vowels = "aeiouAEIOU";
+
+        while (start < end)
+        {
+            while (start < end && vowels.find(word[start]) == string::npos)
+            {
+                start++;
+            }
+
+            while (start < end && vowels.find(word[end]) == string::npos)
+            {
+                end--;
+            }
+
+            swap(word[start], word[end]);
+
+            start++;
+            end--;
+        }
+
+        return word;
+    }
+};
