c6fd8c017eb59f7b8345b549c477f44d06d63f27
Bishal Baira <bishalbaira432@gmail.com>
2024-12-19 15:37:47 +0530

    Add Binary

---
+++ 303_addBinary.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+        string ans;
+        int carry = 0;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+
+        while (i >= 0 || j >= 0 || carry)
+        {
+            if (i >= 0)
+                carry += a[i--] - '0';
+            if (j >= 0)
+                carry += b[j--] - '0';
+            ans += carry % 2 + '0';
+            carry /= 2;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
