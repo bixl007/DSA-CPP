325d76d72469f5839620069738d1a3fa5ed282d7
Bishal Baira <bishalbaira432@gmail.com>
2024-12-21 21:45:20 +0530

    Excel Sheet Column Title

---
+++ 306_excellColTitle.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    string convertToTitle(int columnNumber)
+    {
+        string res = "";
+        while (columnNumber > 0)
+        {
+            columnNumber--;
+            res = char(columnNumber % 26 + 'A') + res;
+            columnNumber /= 26;
+        }
+        return res;
+    }
+};
