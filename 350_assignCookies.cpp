f2349a5abb415ef61801c9d847acfa27b81826df
Bishal Baira <bishalbaira432@gmail.com>
2025-02-04 23:18:37 +0530

    Assign cookies

---
+++ 350_assignCookies.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int findContentChildren(vector<int> &g, vector<int> &s)
+    {
+        int l = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        for (auto i : s)
+        {
+            if (i >= g[l])
+            {
+                l++;
+                if (l == g.size())
+                    break;
+            }
+        }
+        return l;
+    }
+};
