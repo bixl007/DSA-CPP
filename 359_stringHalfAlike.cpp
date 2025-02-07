5cffdfe28790a4abfd0cc3547a85373ec4ececab
Bishal Baira <bishalbaira432@gmail.com>
2025-02-07 22:19:04 +0530

    Add solution for checking if two halves of a string are alike

---
+++ 359_stringHalfAlike.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool halvesAreAlike(string s)
+    {
+        unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u',
+                                      'A', 'E', 'I', 'O', 'U'};
+        int vowelsCount = 0;
+        int midIndex = s.length() / 2;
+
+        for (int i = 0; i < midIndex; i++)
+        {
+            char charA = s[i];
+            char charB = s[midIndex + i];
+            if (vowels.count(charA))
+                vowelsCount++;
+            if (vowels.count(charB))
+                vowelsCount--;
+        }
+
+        return vowelsCount == 0;
+    }
+};
