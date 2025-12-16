2825bcd9c71905b2aa52cf3a4d2112c6fa393a55
Bishal Baira <bishalbaira432@gmail.com>
2025-12-16 15:04:17 +0530

    minimum insertions for palindrome

---
+++ DP/MinInsPalindrome.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string text1, string text2)
+    {
+        int n = text1.size(), m = text2.size();
+        vector<int> prev(m + 1, 0);
+        vector<int> curr(m + 1, 0);
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+
+                if (text1[i - 1] == text2[j - 1])
+                    curr[j] = 1 + prev[j - 1];
+                else
+                    curr[j] = max(prev[j], curr[j - 1]);
+            }
+            prev = curr;
+        }
+
+        return prev[m];
+    }
+    int minInsertions(string s)
+    {
+        string st = s;
+        int n = s.length();
+        reverse(st.begin(), st.end());
+        int len = longestCommonSubsequence(s, st);
+        return n - len;
+    }
+};
